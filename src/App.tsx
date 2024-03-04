05a3285d735a2f08206f319c2c85bce3ba0dd630
AlexisPriser <priseralexis78@gmail.com>
2024-03-04 14:28:41 +0100

    no_e

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import MyComponent from './components/MyComponent';
 function App() {
   return (
     <div className="App">
-      <MyComponent/>e
+      <MyComponent/>
       {/*<Donjon/>*/}
       {/*<TestPar/>*/}
     </div>
